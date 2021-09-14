5259cbb68aae89811866f90a1145c60fa4040774
davelowqx <lowqixiang@gmail.com>
2021-09-15 03:30:23 +0800

    Commented out event listener

--- pages/watch/index.js
+++ pages/watch/index.js
@@ -5,27 +5,33 @@ export default function Watch() {
   const router = useRouter();
   const { v } = router.query;
   const [inverted, setInverted] = React.useState(true);
+  const toggleInvert = () => setInverted(!inverted);
+
   React.useEffect(() => {
-    document?.addEventListener("keydown", (event) => {
-      if (event.repeat) {
-        return;
-      }
-      if (event.code === "KeyF") {
-        console.log("keydown");
-        setInverted(!inverted);
-      }
-    });
+    /*
+    document
+      .getElementById("iframe")
+      .contentWindow.body.addEventListener("keydown", (event) => {
+        if (event.repeat) {
+          return;
+        }
+        if (event.code === "KeyF") {
+          console.log("keydown");
+          toggleInvert();
+        }
+      });
+      */
   }, []);
   return (
     <div className="container">
-      <div>
-        <iframe
-          className={inverted ? "invertedX" : ""}
-          src={`https://www.youtube.com/embed/${v}?autoplay=1&fs=0&loop=1`}
-        ></iframe>
-      </div>
-      <div className="popup">
-        <button onClick={() => setInverted(!inverted)}>FLIP</button>
+      <iframe
+        id="iframe"
+        onClick={toggleInvert}
+        className={inverted ? "invertedX" : ""}
+        src={`https://www.youtube.com/embed/${v}?autoplay=1&fs=0&loop=1`}
+      ></iframe>
+      <div className="overlay">
+        <button onClick={toggleInvert}>{inverted ? "FLOP" : "FLIP"}</button>
       </div>
     </div>
   );
