81f8fb1fb65213d643fd214936059ec9747bd592
davelowqx <lowqixiang@gmail.com>
2021-09-15 00:58:41 +0800

    Added flip button, keydown event listener

--- pages/watch/index.js
+++ pages/watch/index.js
@@ -1,15 +1,31 @@
 import { useRouter } from "next/router";
+import React from "react";
 
 export default function Watch() {
   const router = useRouter();
   const { v } = router.query;
+  const [inverted, setInverted] = React.useState(true);
+  React.useEffect(() => {
+    document?.addEventListener("keydown", (event) => {
+      if (event.repeat) {
+        return;
+      }
+      if (event.code === "KeyF") {
+        console.log("keydown");
+        setInverted(!inverted);
+      }
+    });
+  }, []);
   return (
     <div className="container">
       <div>
         <iframe
-          className="invertedX"
-          src={`https://www.youtube.com/embed/${v}`}
+          className={inverted ? "invertedX" : ""}
+          src={`https://www.youtube.com/embed/${v}?autoplay=1&fs=0&loop=1`}
         ></iframe>
+      </div>
+      <div className="popup">
+        <button onClick={() => setInverted(!inverted)}>FLIP</button>
       </div>
     </div>
   );
